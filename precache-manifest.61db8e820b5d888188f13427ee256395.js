5bb2160438d78465a2aed0bd5417d99215d341fb
Nina <ninarachaebuie@gmail.com>
2019-04-06 22:43:10 -0500

    Updates

---
+++ precache-manifest.61db8e820b5d888188f13427ee256395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a53add1dd4f13155e87ba2d87c5678b6",
+    "url": "/reddit-pokemon-trade-search/static/media/naturebg.a53add1d.jpg"
+  },
+  {
+    "revision": "0a8b1950a48a4ee96ba53ac7963beb5f",
+    "url": "/reddit-pokemon-trade-search/static/media/PokemonDPPt.0a8b1950.ttf"
+  },
+  {
+    "revision": "101d058ff13b4f0ff460",
+    "url": "/reddit-pokemon-trade-search/static/js/runtime~main.2ec9ae61.js"
+  },
+  {
+    "revision": "8a14f6d184fb3384af90",
+    "url": "/reddit-pokemon-trade-search/static/js/main.91fde670.chunk.js"
+  },
+  {
+    "revision": "6fef6e3fb0bc30e52685",
+    "url": "/reddit-pokemon-trade-search/static/js/2.33b7e02d.chunk.js"
+  },
+  {
+    "revision": "8a14f6d184fb3384af90",
+    "url": "/reddit-pokemon-trade-search/static/css/main.e9b11b6c.chunk.css"
+  },
+  {
+    "revision": "31f7cea4bae8e07ef9ecbdbb630722de",
+    "url": "/reddit-pokemon-trade-search/index.html"
+  }
+];
